13e386afabcc9eaf5dc77916d87c1529c1dd76a1
berniehuang2008 <berniehuang2008@163.com>
2025-01-31 14:06:46 +0800

    [feat] 添加文件服务模块，支持本地存储的内容保存与检索 [chore] move `/backend/python` directory to `/src`

---
+++ src/FileService/__init__.py
@@ -0,0 +1,8 @@
+"""
+FileService Module
+
+This is the unified FileSystem Manager Module for the backend.
+All FileSystem operations should be done through this module.
+"""
+
+from . import local
---
+++ src/FileService/local.py
@@ -0,0 +1,55 @@
+"""
+This module provides functions to save and retrieve quark content from local disk storage.
+
+Functions:
+- save_quark_content(quark_id, content): Save quark content to local disk.
+- get_quark_content(quark_id): Get quark content from local disk.
+
+Config:
+- PROJ_ROOT: The root directory of the project (default using cwd, in dev environment).
+
+"""
+import os
+
+# config
+PROJ_ROOT = os.getcwd()    # dev environment only
+QUARK_DIR = f"{PROJ_ROOT}/quarks"
+
+# init dir
+if not os.path.exists(QUARK_DIR):
+    os.makedirs(QUARK_DIR)
+
+# main functions
+def save_quark_content(quark_id, content):
+    """
+    Save quark content to local disk.
+
+    @param quark_id str: The ID of the quark.
+    @param content bytes: The content of the quark, in bytes.
+    @return int: Error code.
+
+    Error codes:
+    0: Success.
+    1: Quark ID conflicts.
+    """
+    if os.path.exists(f"{QUARK_DIR}/{quark_id}"):
+        with open(f"{QUARK_DIR}/{quark_id}", "rb") as f:
+            if f.read() != content:
+                return 1
+            else:
+                return 0
+    
+    with open(f"{QUARK_DIR}/{quark_id}", "wb") as f:
+        f.write(content)
+    return 0
+
+
+def get_quark_content(quark_id):
+    """
+    Get quark content from local disk.
+
+    @param quark_id str: The ID of the quark.
+    @return bytes: The content of the quark, in bytes.
+    """
+    with open(f"{QUARK_DIR}/{quark_id}", "rb") as f:
+        return f.read()
