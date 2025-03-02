1b400ce008aeb4d1eca35e772aa264ec5910a0e8
berniehuang2008 <berniehuang2008@163.com>
2025-03-02 14:42:48 +0800

    cors

--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ import schema
 import FileService
 
 app = flask.Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": ["https://capsule.berniehg.top", "http://capsule.berniehg.top"], "supports_credentials": True}})
 
 def _get_quark_objects(quark_ids: list[str]) -> list[schema.Quark]:
     quarks = []
